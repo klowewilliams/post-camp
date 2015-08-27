1ebf1bb440f3c5e39c839752a70f3bc11911ec50
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-08-26 20:50:07 -0500

    changed the case statement to if statement

--- case_statement4.rb
+++ case_statement4.rb
@@ -5,11 +5,11 @@
 
 print "Enter a string: "
 some_string = gets.chomp
-case
+
 if some_string.match(/\d/)
   puts 'String has numbers'
 elsif some_string.match(/[a-zA-Z]/)
   puts 'String has letters'
-elsif
+else
   puts 'String has no numbers or letters'
 end
