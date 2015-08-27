8c3c6a233572ad935e83609bf9c18ef82b4b74af
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-08-26 20:55:07 -0500

    changed loop to one liner

---
+++ loops_arrays_sets1.rb
@@ -0,0 +1,11 @@
+### One Line Loops, Arrays, and Sets ###
+#Problem:
+#Convert the following ruby loop into one line:
+  # fast_food_restaurants = ["Burger King", "Panera Bread", "Taco Bell", "Chipotle"]
+  # fast_food_restaurants.each do |restaurant|
+  #   puts restaurant
+  # end
+
+##### SOLUTION #######
+fast_food_restaurants = ["Burger King", "Panera Bread", "Taco Bell", "Chipotle"]
+puts fast_food_restaurants
