638f2eec28bf436e04e69298b0144b69fa265ed4
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-08-26 20:30:33 -0500

    used Multi-Value When for case statement exercise 2

--- case_statement2.rb
+++ case_statement2.rb
@@ -6,16 +6,12 @@
 print "Spell out a number (one to five): "
 number = gets.chomp
 case number
-when "one"
-  puts '1'
-when "two"
-  puts '2'
-when "three"
-  puts '3'
-when "four"
-  puts '4'
-when "five"
-  puts '5'
+when "one", "two", "three"
+  puts 'low number'
+when "four", "five"
+  puts 'high number'
 else
   puts "That is not a number between one and five!"
 end
+
+#Multi-Value When
